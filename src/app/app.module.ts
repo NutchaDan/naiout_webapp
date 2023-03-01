8e0e491018e63bbe0e63fb65696002eab8f01e67
NutchaDan <nutcha.siriwut@gmail.com>
2023-03-01 14:12:45 +0700

    add new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailComponent,
+    AddBookComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
